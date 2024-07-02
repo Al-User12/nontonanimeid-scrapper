5158953c57ccf6a3d343a26e8d62c01de9e6ac3f
Al-User12 <alfikrikm2@gmail.com>
2024-07-02 16:26:48 +0700

    chore: Update npm dependencies and add figlet typings

---
+++ src/figlet.ts
@@ -0,0 +1,12 @@
+import figlet from 'figlet';
+
+export function printFiglet(text: string) {
+  figlet(text, (err, data) => {
+    if (err) {
+      console.log('Something went wrong...');
+      console.dir(err);
+      return;
+    }
+    console.log(data);
+  });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import { Hono } from 'hono';
 import { registerRoutes } from './routes';
-
+import { printFiglet } from './figlet';
 const app = new Hono();
 
 registerRoutes(app);
 
+printFiglet('Made with Hono and Bun!');
+
 app.fire();
+
+console.log('📌 this is a Rest API to scrape anime list from nontonanimeid.baby and ❤️ From Al-User12 ');
 
 export default app;
